bf86ee4923770485f19f74f633efce7de7cd4e70
Jonathan Augusto Jorge <jonathan.augusto.jorge19@gmail.com>
2020-11-22 15:10:23 -0300

    Update whats.py

--- whats.py
+++ whats.py
@@ -1,6 +1,6 @@
 import pywhatkit
 
 try:
-    pywhatkit.sendwhatmsg("+5511948433738", "this teste", 13-13)
+    pywhatkit.sendwhatmsg("+5511********", "this teste", 13-13)
 except:
     print("An Unexpected Error!")
